8ae24aaeafae56ed52d2cc715db1c30dbed261f3
patrickletran <patrickjmvtran@gmail.com>
2022-10-13 10:29:15 -0700

    10:28 October 13 Commit

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedInPlaceWithNums() {
+    int[] input1 = {1,2,4};
+    assertArrayEquals(new int[]{4,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedWithNums() {
+    int[] input1 = {1,2,4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,2,1}, input1);
+  }
 }
